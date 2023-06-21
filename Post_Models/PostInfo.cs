b74445e1b36fd6f5f75a0af78b2fb5bcb70fc661
Jm Dinglasan <116419870+JmDinglasan@users.noreply.github.com>
2023-06-21 16:37:04 +0800

    Update PostInfo.cs

--- Post_Models/PostInfo.cs
+++ Post_Models/PostInfo.cs
@@ -8,5 +8,15 @@ namespace Post_Models
 {
     internal class PostInfo
     {
+        public int PostNumber { get; set; }
+        public string Content { get; set; }
+        public DateTime DateTimePosted { get; set; }
+
+        public PostInfo(int postNumber, string content)
+        {
+            PostNumber = postNumber;
+            Content = content;
+            DateTimePosted = DateTime.Now;
+        }
     }
 }
